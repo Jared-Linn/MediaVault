32231582dec64aff971c9ed4771206f06e7ea9b5
Jared-Linn <qq3297848041@163.com>
2024-11-13 21:34:32 +0800

    自定义一些信息状态

--- mediavault-spring/src/main/java/com/linqiumeng/mediavault/utils/MessageConstant.java
+++ mediavault-spring/src/main/java/com/linqiumeng/mediavault/utils/MessageConstant.java
@@ -4,6 +4,23 @@ package com.linqiumeng.mediavault.utils;
  * MessageConstant 类通常用于存储常量消息，以便在多个地方复用这些消息
  */
 public class MessageConstant {
+    // 状态码
+    public static final int MSG_200 = 200;
+    public static final int MSG_201 = 201;
+    public static final int MSG_202 = 202;
+    public static final int MSG_203= 203;
+    public static final int MSG_204 = 204;
+    public static final int MSG_205 = 205;
+    public static final int MSG_206= 206;
+    public static final int MSG_208= 208;
+    public static final int MSG_226= 226;
+    public static final int MSG_400 = 400;
+    public static final int MSG_401 = 401;
+    public static final int MSG_403 = 403;
+    public static final int MSG_404 = 404;
+    public static final int MSG_405 = 405;
+    public static final int MSG_500 = 500;
+    public static final int MSG_501 = 501;
     public static final String UPLOAD_FAILED = "文件上传失败";
     public static final String SUCCESS = "操作成功";
     public static final String FAILURE = "操作失败";
