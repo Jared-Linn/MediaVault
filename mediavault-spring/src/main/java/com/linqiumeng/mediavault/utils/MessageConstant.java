48bd718a92b17104fe6817dfb1eb1b23fa24e6f0
Jared-Linn <qq3297848041@163.com>
2024-11-13 09:48:09 +0800

    Create MessageConstant.java
    MessageConstant 类通常用于存储常量消息，以便在多个地方复用这些消息

---
+++ mediavault-spring/src/main/java/com/linqiumeng/mediavault/utils/MessageConstant.java
@@ -0,0 +1,14 @@
+package com.linqiumeng.mediavault.utils;
+
+/**
+ * MessageConstant 类通常用于存储常量消息，以便在多个地方复用这些消息
+ */
+public class MessageConstant {
+    public static final String UPLOAD_FAILED = "文件上传失败";
+    public static final String SUCCESS = "操作成功";
+    public static final String FAILURE = "操作失败";
+    // 其他常量消息
+
+    public static final String INVALID_FILE_NAME = "Invalid file name";
+    public static final String INVALID_FILE_EXTENSION = "Invalid file extension";
+}
