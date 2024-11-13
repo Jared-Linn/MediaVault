e66fe530d98e69f1888f4dc659762fea8370b383
Jared-Linn <qq3297848041@163.com>
2024-11-13 09:49:29 +0800

    封装文件上传到OSS
    封装文件上传到OSS的逻辑。

---
+++ mediavault-spring/src/main/java/com/linqiumeng/mediavault/utils/AliOssUtil.java
@@ -0,0 +1,69 @@
+package com.linqiumeng.mediavault.utils;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+
+/**
+ * 工具类
+ * 封装文件上传到OSS的逻辑。
+ */
+@Slf4j
+@Component
+public class AliOssUtil {
+
+    @Value("${ali.oss.endpoint}")
+    private String endpoint;
+
+    @Value("${ali.oss.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${ali.oss.access-key-secret}")
+    private String accessKeySecret;
+
+    @Value("${ali.oss.bucket-name}")
+    private String bucketName;
+
+    /**
+     * 文件上传
+     *
+     * @param bytes
+     * @param objectName
+     * @return
+     */
+    public String upload(byte[] bytes, String objectName) {
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            // 创建PutObject请求。
+            ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(bytes));
+        } catch (OSSException oe) {
+            log.error("Caught an OSSException, which means your request made it to OSS, but was rejected with an error response for some reason.", oe);
+            log.error("Error Message: {}", oe.getErrorMessage());
+            log.error("Error Code: {}", oe.getErrorCode());
+            log.error("Request ID: {}", oe.getRequestId());
+            log.error("Host ID: {}", oe.getHostId());
+        } catch (ClientException ce) {
+            log.error("Caught an ClientException, which means the client encountered a serious internal problem while trying to communicate with OSS, such as not being able to access the network.", ce);
+            log.error("Error Message: {}", ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+
+        // 文件访问路径规则
+        String fileUrl = String.format("https://%s.%s/%s", bucketName, endpoint, objectName);
+
+        log.info("文件上传到: {}", fileUrl);
+
+        return fileUrl;
+    }
+}
