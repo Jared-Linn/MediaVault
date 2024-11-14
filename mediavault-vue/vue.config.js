c89e8778ff61d0c659d343fc5fbb37f6f726d216
Jared-Linn <qq3297848041@163.com>
2024-11-14 21:24:04 +0800

    Update vue.config.js

--- mediavault-vue/vue.config.js
+++ mediavault-vue/vue.config.js
@@ -10,13 +10,13 @@ module.exports = defineConfig({
 
   devServer: {
     host: '0.0.0.0',
-    port: 8000,
+    port: 3000,
     proxy: {
 
       '/api': {
         // target: "http://103.207.68.161:6741",
         // target: "http://172.10.30.20:8081",
-        target: "http://172.10.30.20:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
