b193820e462659d642d77baa3772f1367f5ef8a0
Jared-Linn <qq3297848041@163.com>
2024-11-14 17:31:24 +0800

    Update vue.config.js

--- mediavault-vue/vue.config.js
+++ mediavault-vue/vue.config.js
@@ -15,7 +15,8 @@ module.exports = defineConfig({
 
       '/api': {
         // target: "http://103.207.68.161:6741",
-        target: "http://172.10.30.20:8081",
+        // target: "http://172.10.30.20:8081",
+        target: "http://172.10.30.20:5000",
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
